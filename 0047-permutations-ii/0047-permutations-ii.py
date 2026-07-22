f04d709f7c7198cc487947b01b1e8cb1a108ce40
seshudeepika08 <seshudeepika08@gmail.com>
2026-07-22 09:51:16 +0530

    Time: 11 ms (38.54%), Space: 12.9 MB (74.78%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def permuteUnique(self, nums):
+        nums.sort()          # Sort to handle duplicates
+        result = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                # Skip if already used
+                if used[i]:
+                    continue
+
+                # Skip duplicate elements
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return result
